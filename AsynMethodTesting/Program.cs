00e3208a36fef9e30351246c3e030d7e1a7d55f1
awang3 <awang2@ra.rockwell.com>
2017-04-13 18:08:37 +0800

    Init

---
+++ AsynMethodTesting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynMethodTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public async Task MyMethod()
+        {
+            //throw new InvalidOperationException();
+            await Task.Delay(1000);
+        }
+
+        public async Task CallMethod()
+        {
+            await MyMethod();
+        }
+
+        public async void ThrowExceptionAsync()
+        {
+            throw new InvalidOperationException();
+        }
+
+        public void AsyncVoidException_CannotBeCaughtByCatch()
+        {
+            try
+            {
+                ThrowExceptionAsync();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
